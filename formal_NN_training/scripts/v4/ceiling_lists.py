d7a877e9989d9cc63a53ae101a4dac0dc8563673
Angelawoo572 <angelawoo571@gmail.com>
2026-07-04 22:25:07 -0400

    Document v4 ceiling list command

--- formal_NN_training/scripts/v4/ceiling_lists.py
+++ formal_NN_training/scripts/v4/ceiling_lists.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python3
-# v4 ceiling list entrypoint
+"""Use formal_NN_training/scripts/19_build_oracle_ceiling_lists.py."""
+print("Use formal_NN_training/scripts/19_build_oracle_ceiling_lists.py")
